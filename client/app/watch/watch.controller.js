ed319410731a26b3e3cc67091a3dbae10a90882f
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2014-07-05 21:25:15 +0700

    Finish saving video

--- client/app/watch/watch.controller.js
+++ client/app/watch/watch.controller.js
@@ -4,13 +4,14 @@ angular.module('bayhackApp')
   .controller('WatchCtrl', function ($scope, $http, socket) {
     $scope.message = 'Hello';
 
-    $http.get('/api/chats').success(function(chats) {
+    $http.get('/api/chats').success(function (chats) {
       $scope.chats = chats;
       socket.syncUpdates('chat', $scope.chats);
       scrollFn();
     });
 
     var webrtc = new SimpleWebRTC({
+      debug: false,
       // the id/element dom element that will hold "our" video
       localVideoEl: 'localVideo',
       // the id/element dom element that will hold remote videos
@@ -19,43 +20,46 @@ angular.module('bayhackApp')
       autoRequestMedia: true
     });
 
+    var videoRTC;
+    var audioRTC;
+
     // we have to wait until it's ready
     webrtc.on('readyToCall', function () {
       // you can name it anything
       webrtc.joinRoom('testing');
+      setTimeout(function(){
+        var localMediaStream = webrtc.getLocalStream();
+        videoRTC = RecordRTC(localMediaStream, { type: 'video' });
+        videoRTC.startRecording();
+        audioRTC = RecordRTC(localMediaStream, { type: 'audio' });
+        audioRTC.startRecording();
+      },100);
     });
 
-//    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-//
-//    var constraints = {video: true, audio: true};
-//
-//    function successCallback(stream) {
-//      var video = document.querySelector("video");
-//      video.src = window.URL.createObjectURL(stream);
-//      video.play();
-//    }
-//
-//    function errorCallback(error) {
-//      console.log("navigator.getUserMedia error: ", error);
-//    }
-//
-//    navigator.getUserMedia(constraints, successCallback, errorCallback);
-
-    var scrollFn = function(){
-      setTimeout(function(){
+    $scope.stopRecord = function(){
+      videoRTC.stopRecording(function(videoURL){
+        audioRTC.stopRecording(function(audioURL) {
+          console.log("videoURL:"+videoURL);
+          console.log("audioURL:"+audioURL);
+          webrtc.leaveRoom();
+        });
+      });
+    }
+    var scrollFn = function () {
+      setTimeout(function () {
         $("#chatbox").scrollTop($("#chatbox")[0].scrollHeight);
-      },10);
+      }, 10);
     }
 
-    $scope.$watch('chats.length', function(v){
+    $scope.$watch('chats.length', function (v) {
       scrollFn();
     });
 
-    $scope.addChat = function() {
-      if($scope.newChat === '') {
+    $scope.addChat = function () {
+      if ($scope.newChat === '') {
         return;
       }
-      $http.post('/api/chats', { who:'Somebody', message:$scope.newChat });
+      $http.post('/api/chats', { who: 'Somebody', message: $scope.newChat });
       $scope.newChat = '';
       scrollFn();
     };
