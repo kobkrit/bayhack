d25689895b65fbffb54fa090219f071760ee17e0
Kobkrit Viriyayudhakorn <kobkrit@gmail.com>
2014-07-05 23:08:44 +0700

    Finish merge files

--- client/app/watch/watch.controller.js
+++ client/app/watch/watch.controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('bayhackApp')
-  .controller('WatchCtrl', function ($scope, $http, socket) {
-    $scope.message = 'Hello';
+  .controller('WatchCtrl', function ($scope, $http, socket, Auth) {
+    $scope.isTutor = Auth.isTutor();
 
     $http.get('/api/chats').success(function (chats) {
       $scope.chats = chats;
@@ -27,28 +27,66 @@ angular.module('bayhackApp')
     webrtc.on('readyToCall', function () {
       // you can name it anything
       webrtc.joinRoom('testing');
-      $scope.$apply(function () {
-        $scope.status = "Start Recording...";
-      });
-      setTimeout(function () {
-        var localMediaStream = webrtc.getLocalStream();
-        videoRTC = RecordRTC(localMediaStream, { type: 'video' });
-        videoRTC.startRecording();
-        audioRTC = RecordRTC(localMediaStream, { type: 'audio' });
-        audioRTC.startRecording();
+      if ($scope.isTutor) {
         $scope.$apply(function () {
-          $scope.status = "Recording...";
+          $scope.status = "Start Recording...";
         });
-      }, 100);
+        setTimeout(function () {
+          var localMediaStream = webrtc.getLocalStream();
+          videoRTC = RecordRTC(localMediaStream, { type: 'video' });
+          videoRTC.startRecording();
+          audioRTC = RecordRTC(localMediaStream, { type: 'audio' });
+          audioRTC.startRecording();
+          $scope.$apply(function () {
+            $scope.status = "Recording...";
+          });
+        }, 100);
+      }
     });
 
     $scope.$on('$destroy', function () {
-      if (!window.confirm("Are you sure you want to leave this page? Your record will be gone.")) {
-        event.preventDefault();
-      }else{
-        socket.unsyncUpdates('chat');
-      }
+//      if (!window.confirm("Are you sure you want to leave this page? Your record will be gone.")) {
+//        event.preventDefault();
+//      }else{
+      socket.unsyncUpdates('chat');
+//      }
     });
+
+    // generating random string
+    function generateRandomString() {
+      if (window.crypto) {
+        var a = window.crypto.getRandomValues(new Uint32Array(3)),
+          token = '';
+        for (var i = 0, l = a.length; i < l; i++) token += a[i].toString(36);
+        return token;
+      } else {
+        return (Math.random() * new Date().getTime()).toString(36).replace(/\./g, '');
+      }
+    }
+
+    function submitToServer(audio, video) {
+      // getting unique identifier for the file name
+      var fileName = generateRandomString();
+
+      // this object is used to allow submitting multiple recorded blobs
+      var files = { };
+      // recorded audio blob
+      files.audio = {
+        name: fileName + '.' + audio.blob.type.split('/')[1],
+        type: audio.blob.type,
+        contents: audio.dataURL
+      };
+
+      files.video = {
+        name: fileName + '.' + video.blob.type.split('/')[1],
+        type: video.blob.type,
+        contents: video.dataURL
+      };
+
+      $http.post('/upload', JSON.stringify(files)).success(function(uploads){
+        console.log(uploads);
+      });
+    }
 
     $scope.stopRecord = function () {
       videoRTC.stopRecording(function (videoURL) {
@@ -56,6 +94,21 @@ angular.module('bayhackApp')
           console.log("videoURL:" + videoURL);
           console.log("audioURL:" + audioURL);
           webrtc.leaveRoom();
+
+          audioRTC.getDataURL(function (audioDataURL) {
+            var audio = {
+              blob: audioRTC.getBlob(),
+              dataURL: audioDataURL
+            };
+            videoRTC.getDataURL(function (videoDataURL) {
+              var video = {
+                blob: videoRTC.getBlob(),
+                dataURL: videoDataURL
+              };
+              submitToServer(audio, video);
+            });
+          });
+
           $scope.$apply(function () {
             $scope.status = "Stopped!";
           });
--- server/routes.js
+++ server/routes.js
@@ -5,16 +5,85 @@
 'use strict';
 
 var errors = require('./components/errors');
+var fs = require('fs');
+
+function _upload(response, file) {
+  var fileRootName = file.name.split('.').shift(),
+    fileExtension = file.name.split('.').pop(),
+    filePathBase = __dirname +'/uploads/',
+    fileRootNameWithBase = filePathBase + fileRootName,
+    filePath = fileRootNameWithBase + '.' + fileExtension,
+    fileID = 2,
+    fileBuffer;
+
+  while (fs.existsSync(filePath)) {
+    filePath = fileRootNameWithBase + '(' + fileID + ').' + fileExtension;
+    fileID += 1;
+  }
+
+  file.contents = file.contents.split(',').pop();
+
+  fileBuffer = new Buffer(file.contents, "base64");
+
+  fs.writeFileSync(filePath, fileBuffer);
+}
+
+function ifMac(response, files) {
+  // its probably *nix, assume ffmpeg is available
+  var audioFile = __dirname + '/uploads/' + files.audio.name;
+  var videoFile = __dirname + '/uploads/' + files.video.name;
+  var mergedFile = __dirname + '/uploads/' + files.audio.name.split('.')[0] + '-merged.webm';
+
+  var util = require('util'),
+    exec = require('child_process').exec;
+
+  var command = "ffmpeg -i " + audioFile + " -i " + videoFile + " -map 0:0 -map 1:0 " + mergedFile;
+
+  exec(command, function (error, stdout, stderr) {
+    if (stdout) console.log(stdout);
+    if (stderr) console.log(stderr);
+
+    if (error) {
+      console.log('exec error: ' + error);
+      response.statusCode = 404;
+      response.end();
+
+    } else {
+      response.statusCode = 200;
+      response.writeHead(200, {
+        'Content-Type': 'application/json'
+      });
+      response.end(files.audio.name.split('.')[0] + '-merged.webm');
+
+      // removing audio/video files
+      fs.unlink(audioFile);
+      fs.unlink(videoFile);
+    }
+
+  });
+}
 
 module.exports = function(app) {
-
   // Insert routes below
   app.use('/api/chats', require('./api/chat'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
 
   app.use('/auth', require('./auth'));
-  
+
+  //Nattaphong zone
+  app.get('/pay', function(req,res,next){
+    //req.body
+    res.end("ok");
+  });
+
+  app.post('/upload', function(req,res,next){
+    console.dir(req.body);
+    _upload(res, req.body.audio);
+    _upload(res, req.body.video);
+    ifMac(res, req.body);
+  });
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
